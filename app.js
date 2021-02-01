eb3186d15db6efd807c4e5ad7a92b0c0bd272d63
Amydin S <amydin17@gmail.com>
2021-02-01 10:09:52 +0700

    add scrape parameters for start date and range of days

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.set('view engine', 'pug');
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '512mb' }));
+app.use(express.urlencoded({ extended: true, limit: '512mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/info.js
+++ routes/info.js
@@ -3,72 +3,12 @@ var router = express.Router();
 
 const _ = require('lodash');
 const log = require('debug')('info');
+const moment = require('moment');
 const pup = require('puppeteer');
-const superagent = require('superagent');
+const axios = require('axios');
 
 const { PWD, NODE_ENV } = process.env;
 const useragent = require(`${PWD}/data/user-agent.json`);
-
-function _expedia_info(account) {
-    return new Promise(async (resolve, reject) => {
-        if( _.isNil(account) || _.isNil(account.url) || _.isEmpty(account.url) || _.isNil(account.webhookUrl) || _.isEmpty(account.webhookUrl) ) {
-            return reject({
-                status : false,
-                message: "Check your parameters data"
-            })
-        }
-
-        const browser = await pup.launch({ headless: _.includes(["production", "staging"], NODE_ENV) ? true : false, 
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--disable-gpu',
-        ]});
-        const ua = useragent[ _.random(0, _.size(useragent)-1) ];
-        if( !_.includes(["production", "staging"], NODE_ENV) ) log("ua: %s", ua);
-        
-        const pages = await browser.pages();
-        const page  = pages[0];
-
-        // Check response continuously till browser close
-        page.on('response', async (res) => {
-            try {
-                if( 
-                    res.url().includes('/graphql') && 
-                    res.request().method() == 'POST' && 
-                    res.request().postData().includes('PropertyInfoOffersBexFeatures') 
-                ) {
-                    const body = await res.json();
-                    console.log('body:', body);
-                    await superagent.post(account.webhookUrl).send(body).set('accept', 'json');
-                }
-            } catch (err) {
-                console.error(res, err);
-                console.log(res, err);
-            }
-        });
-
-        try {
-            await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36");
-            await page.goto(account.url);
-
-            if( _.includes(["production", "staging"], NODE_ENV) ) await browser.close();
-            return resolve({
-                status : true,
-                message: "Check the usage of your webhook URL continuously, the server is scraping..."
-            })
-        } catch (err) {
-            if( !_.includes(["production", "staging"], NODE_ENV) ) log('err: %s', err);
-            if( _.includes(["production", "staging"], NODE_ENV) ) await browser.close();
-            return reject({
-                status : false,
-                message: err
-            })
-        }
-    })
-}
 
 router.post('/', async function(req, res) {
     try {
@@ -83,3 +23,109 @@ router.post('/', async function(req, res) {
 });
 
 module.exports = router;
+
+function _expedia_info(account) {
+    return new Promise(async (resolve, reject) => {
+        if( _.isNil(account) || _.isNil(account.url) || _.isEmpty(account.url) || _.isNil(account.webhookUrl) || _.isEmpty(account.webhookUrl) || _.isNil(account.rangeOfDays) ) {
+            return reject({
+                status : false,
+                message: "Check your parameters data"
+            })
+        }
+
+        let rangeOfDays = _.toInteger(account.rangeOfDays)-1 || 1;
+        if( _.isNil(account.startDate) || _.isEmpty(account.startDate) || (!_.isNil(account.startDate) && !moment(account.startDate).isValid()) ) account.startDate = moment().subtract(1, 'day').format('YYYY-MM-DD');
+        else account.startDate = moment(account.startDate).subtract(1, 'day').format('YYYY-MM-DD');
+        account.endDate = moment(account.startDate).add(1, 'day').format('YYYY-MM-DD');
+
+        const browser = await pup.launch({ headless: _.includes(["production", "staging"], NODE_ENV) ? true : false, 
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu',
+            '--incognito',
+        ]});
+        const ua = useragent[ _.random(0, _.size(useragent)-1) ];
+        if( !_.includes(["production", "staging"], NODE_ENV) ) log("ua: %s", ua);
+        
+        let   hotelName = null;
+        const [page]    = await browser.pages();
+        try {
+            let [url] = _.split(account.url, '?');
+                url   = `${url}?chkin=${account.startDate}&chkout=${account.endDate}`;
+
+            await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36");
+            await page.goto(url, { waitUntil: ['load', 'domcontentloaded'], timeout: 0 });
+
+            // Get hotel's name
+            [hotelName] = await page.$x('//div[@data-stid="content-hotel-title"]/h1');
+            if( hotelName ) {
+                hotelName = await hotelName.getProperty('textContent');
+                hotelName = await hotelName.jsonValue();
+            }
+
+            await page.waitFor( _.random(4000, 5000) );
+            await page.waitForXPath('//button[@data-stid="date-stepper-button-check-in-next" and @disabled]', { hidden: true, timeout: 20000 });
+            // The first day
+            const [firstday] = await page.$x('//button[@data-stid="date-stepper-button-check-in-next"]');
+            await firstday.click();
+            rangeOfDays -= 1;
+        } catch (err) {
+            log(err);
+            if( _.includes(["production", "staging"], NODE_ENV) ) await browser.close();
+            return reject({
+                status : false,
+                message: err
+            })
+        }
+
+        // Check response continuously till browser close
+        page.on('response', async (res) => {
+            try {
+                if( 
+                    res.url().includes('/graphql') && 
+                    res.request().method() == 'POST' && 
+                    res.request().postData().includes('PropertyInfoOffersBexFeatures') 
+                ) {
+                    await page.waitFor( _.random(1000, 2000) );
+                    const body = await res.json();
+                    
+                    result = {
+                        name  : hotelName,
+                        search: body.data.propertyInfo.offers.searchCriteria,
+                        rooms : body.data.propertyInfo.offers.listings
+                    };
+
+                    await axios({
+                        url             : account.webhookUrl,
+                        method          : "post",
+                        data            : result,
+                        timeout         : 0,
+                        maxContentLength: Infinity,
+                        maxBodyLength   : Infinity
+                    });
+
+                    if( _.gte(rangeOfDays, 0) ) {
+                        await page.waitFor( _.random(2000, 3000) );
+                        await page.waitForXPath('//button[@data-stid="date-stepper-button-check-in-next" and @disabled]', { hidden: true, timeout: 20000 });
+                        const [nextday] = await page.$x('//button[@data-stid="date-stepper-button-check-in-next"]');
+                        await nextday.click();
+                        rangeOfDays -= 1;
+                        return;
+                    };
+                    if( _.includes(["production", "staging"], NODE_ENV) ) await browser.close();
+                }
+            } catch (err) {
+                log(err);
+                if( _.includes(["production", "staging"], NODE_ENV) ) await browser.close();
+            }
+        });
+
+        return resolve({
+            status : true,
+            message: "Check the usage of your webhook URL continuously, the server is scraping..."
+        })
+    })
+}
